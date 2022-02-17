18f1f5e31df94690a90eb5d785f7bb546e0c9f61
Andrews-repo <andrewpatty3@gmail.com>
2022-02-17 10:19:52 -0800

    first AWS terraform Server

---
+++ main.tf
@@ -0,0 +1,80 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+# create a server
+resource "aws_instance" "test-terra-server" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "2nd-key-pair"
+  vpc_security_group_ids = ["${aws_security_group.allow_web.id}"]
+
+  user_data = <<-EOF
+               #!/bin/bash
+               sudo apt update -y
+               sudo apt install apache2 -y
+               sudo systemctl start apache2
+               sudo bash -c 'echo your first terraform server > /var/www/html/index.html'
+               EOF
+  tags = {
+    Name = "web server"
+  }       
+}
+
+# Create a security group to allow port 22,80,443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
