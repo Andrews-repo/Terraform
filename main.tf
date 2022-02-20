0009f2af9ff77110480b4fdf0e3c49cc7f9459f7
Andrews-repo <andrewpatty3@gmail.com>
2022-02-20 12:38:25 -0800

    Jenkins
    Script that builds a Jenkins server & EC2 role

--- main.tf
+++ main.tf
@@ -10,34 +10,64 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIAQRNJR6LVRTNQTBSO"
+  secret_key = "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"
 }
 
-# create a server
-resource "aws_instance" "test-terra-server" {
-  ami           = "ami-04505e74c0741db8d"
-  instance_type = "t2.micro"
-  availability_zone = "us-east-1a"
-  key_name = "2nd-key-pair"
-  vpc_security_group_ids = ["${aws_security_group.allow_web.id}"]
-
-  user_data = <<-EOF
-               #!/bin/bash
-               sudo apt update -y
-               sudo apt install apache2 -y
-               sudo systemctl start apache2
-               sudo bash -c 'echo your first terraform server > /var/www/html/index.html'
-               EOF
+# create VPC
+resource "aws_vpc" "prod_vpc" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "web server"
-  }       
+      Name = "Production"
+  }
 }
 
+# create Internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod_vpc.id
+}
+
+# create route table
+
+resource "aws_route_table" "prod_route_table" {
+  vpc_id = aws_vpc.prod_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod"
+  }
+}
+
+# create a subnet
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.prod_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+# associate subnet with Route Table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod_route_table.id
+}
 # Create a security group to allow port 22,80,443
 resource "aws_security_group" "allow_web" {
   name        = "allow_web_traffic"
   description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.prod_vpc.id
 
   ingress {
     description      = "HTTPS"
@@ -77,4 +107,45 @@ resource "aws_security_group" "allow_web" {
   tags = {
     Name = "allow_web"
   }
+}
+
+# create a network interface with an ip in the subnet that was created in step 4
+resource "aws_network_interface" "web_server_nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+# assign an elastic ip to the network interface created in step 7
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web_server_nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.gw]
+}
+
+# create a server
+
+resource "aws_instance" "test-terra-server" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "2nd-key-pair"
+
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.web_server_nic.id
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo your first terraform server > /var/www/html/index.html'
+                EOF
+  tags = {
+    Name = "web server"
+  }       
 }
